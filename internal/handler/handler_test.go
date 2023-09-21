b3c7f2807101a690f03162a2a8b42a89bfab5487
Anton Kuzmin <kzmn.a.d@gmail.com>
2023-09-21 13:01:48 +0200

    implemented initial get contents handler

---
+++ internal/handler/handler_test.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestGetContents(t *testing.T) {
+	router := gin.Default()
+	Routes(router)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/v1/contents", nil)
+	router.ServeHTTP(w, req)
+
+	if w.Code != 200 {
+		t.Errorf("GET /v1/contents returns code %s, should 200", strconv.Itoa(w.Code))
+	}
+}
